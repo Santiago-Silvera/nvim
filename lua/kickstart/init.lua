5e35fd8fc98ed6a6da011f6fa9d9e2a475e7642e
Elizabeth Flores <santiagosil3434@gmail.com>
2024-01-08 01:15:45 -0300

    de-bloating init.lua

--- lua/kickstart/init.lua
+++
@@ -1,6 +0,0 @@
--- Do NOT include autoformatter, it is already required in global init.lua
--- require('kickstart.autoformatter')
-require('kickstart.remap')
-require('kickstart.gitsign')
-require('kickstart.trouble')
-return {}
